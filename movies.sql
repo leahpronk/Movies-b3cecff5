adb3b8b95e6a787e4b02316c83f032dedc94ff24
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2020-03-06 09:44:29 +0100

    Uitwerking van de exercise

---
+++ movies.sql
@@ -0,0 +1,22 @@
+use `netland`;
+
+-- CREATE TABLE `films` (
+-- id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- titel VARCHAR(100) NOT NULL,
+-- duur int(5) NOT NULL,
+-- datum_uitkomst DATE,
+-- herkomst VARCHAR(100) NOT NULL,
+-- omschrijving VARCHAR(100) NOT NULL,
+-- YouTube VARCHAR(100) NOT NULL
+-- );
+
+
+INSERT INTO `films` (`titel`, `duur`, `datum_uitkomst`, `herkomst`, `omschrijving`, `Youtube`) VALUES 
+('Star Wars: The Rise of Skywalker', 6.9, 'When its discovered that the evil Emperor Palpatine did not die at the hands of Darth Vader, the rebels must race against the clock to find out his whereabouts. Finn and Poe lead the Resistance to put a stop to the First Orders plans to form a new Empire, while Rey anticipates her inevitable confrontation with Kylo Ren.', 'YGj4UcBEhf8'),
+('Monty Python and the Holy Grail', 8.2, 'A comedic send-up of the grim circumstances of the Middle Ages as told through the story of King Arthur and framed by a modern-day murder investigation. When the mythical king of the Britons leads his knights on a quest for the Holy Grail, they face a wide array of horrors, including a persistent Black Knight, a three-headed giant, a cadre of shrubbery-challenged knights, the perilous Castle Anthrax, a killer rabbit, a house of virgins, and a handful of rude Frenchmen.', 'EJdzuwSF-Fc', 7),
+('Monty Pythons The Meaning of Life', 7.6, 'The stages of life are told through multiple sketches and songs by the British comedy troupe. The seven parts of life cover birth, growing up, war, middle age, organ transplants, old age and death. Not all stages are singular: "Part I: The Miracle of Birth" is from the perspective of an ignored woman in labor, and of a Roman Catholic couple with too many children, and "Part VII: Death" encompasses a funeral and heaven. Added are three unrelated skits placed in the beginning, middle and end.'),
+
+
+
+
+describe series;
